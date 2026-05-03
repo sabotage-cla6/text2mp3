af4ef5712dcdab4aed14e4ebc0ba9e74d8f284c1
sabotage-cla6 <nine.gates111999@mail.com>
2026-05-04 08:20:15 +0900

    スクリプトの修正

--- bin/text2mp3.ps1
+++ bin/text2mp3.ps1
@@ -1,37 +1,52 @@
-param($volumn, $i, $o, $d, $s, $showvoices)
+param($v, $i, $o, $d, [switch]$l)
 
-$inputfile = $i
-$output = $o
-$dict = $d
-$srtfile = $s
-
-if ($null -ne $showvoices -and $showvoices -ne "") {
-    podman run `
-        --rm -it `
-        -v ${volumn}:/tmp/volumn:Z `
-        sabotagecla6/text2mp3 `
-        /bin/bash /usr/local/text2mp3/bin/edge-tts --list-voices | grep "${showvoices}"
+if ($l) {
+    podman run --rm -it sabotagecla6/text2mp3 /bin/bash /usr/local/text2mp3/text2mp3.sh edge-tts --list-voices
     exit 0
 }
-else{
+
+# 
+$infile = $((Get-Item $i).BaseName)
+$outfile = $o
+
+# 作業用フォルダの作成
+$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+$randomString = -join ((1..12) | ForEach-Object { $chars[(Get-Random -Maximum $chars.Length)] })
+$tempdir="$($PSScriptRoot)\..\tmp\${randomString}"
+mkdir $tempdir
+$tempdir = Resolve-Path $tempdir
+
 if ($null -eq $volumn -or $volumn -eq "") {
-    $volumn = ".\"
+    $volumn = Resolve-Path "./"
 }
 
-if ($null -eq $srtfile -or $srtfile -eq "") {
-    $srtfile = $output -replace "\.mp3$", ".srt"
+Copy-Item -Path $i -Destination "${tempdir}\infile.yaml"
+if($null -ne $d -and $d -ne ""){
+    Copy-Item -Path $d -Destination "${tempdir}\dictfile.yaml"
 }
-if($null -ne $dict -or $dict -ne "") {
-    $dict = "dict.yaml"
+if (Test-Path "$($PSScriptRoot)\..\config\global-dict.yaml"){
+    Copy-Item -Path "$($PSScriptRoot)\..\config\global-dict.yaml" -Destination "${tempdir}\global-dict.yaml"
+}
+if ( $null -eq "$outfile" -or "$outfile" -eq ""){
+    $outfile="/tmp/volumn/${infile}.mp3"
 }
 
-podman run `
-    --rm -it `
-    -v ${volumn}:/tmp/volumn:Z `
+$args_str=""
+if ( $null -ne "$outfile" -and "$outfile" -ne ""){
+    $args_str="${args_str} -o /tmp/volumn/${infile}.mp3"
+}
+if($null -ne $d -and $d -ne ""){
+    $args_str="${args_str} -d /tmp/data/dictfile.yaml"
+}
+if (Test-Path "${tempdir}\global-dict.yaml"){
+    $args_str="${args_str} -g /tmp/data/global-dict.yaml"
+}
+
+podman run --rm -it `
+    -v "${volumn}:/tmp/volumn:Z" `
+    -v "${tempdir}:/tmp/data/" `
     sabotagecla6/text2mp3 `
-    /bin/bash /usr/local/text2mp3/text2mp3.sh `
-    -i "/tmp/volumn/${inputfile}" `
-    -o "/tmp/volumn/${output}" `
-    -d "/tmp/volumn/${dict}" `
-    -s "/tmp/volumn/${srtfile}"
-}
+    /bin/bash /usr/local/text2mp3/text2mp3.sh python3 main.py `
+    -i "/tmp/data/infile.yaml" ${args_str}
+
+Remove-Item -Force -Recurse $tempdir
